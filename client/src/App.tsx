3ab2ac4067ba4707f8006f0fb008e02bd7ada171
Callum <callum@clow.nz>
2024-10-29 22:40:09 +1300

    Basic components started for adding im bringing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import CreateImBringing from "./components/CreateImBringing";
 
 function App() {
-
   return (
     <>
-     
+      <CreateImBringing />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
