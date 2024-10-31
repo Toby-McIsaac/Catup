0574048abde032013b16198caebe4e671b22c2b4
Callum <callum@clow.nz>
2024-10-31 15:25:15 +1300

    extracted component toggle button into its own component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import CreateImBringing from "./pages/ImBringing/ImBringingButton";
+import ImBringing from "./pages/ImBringing/ImBringing";
 
 function App() {
   return (
     <>
-      <CreateImBringing />
+      <ImBringing />
     </>
   );
 }
